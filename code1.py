5125345ec42faa8a60075ea0509ad2f9f13c6874
LLAGONNE <nathan.llagonne@eivp-paris.fr>
2020-11-14 11:10:35 +0100

    changement des séparateurs et premier plot

--- code1.py
+++ code1.py
@@ -2,6 +2,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 #J'importe les 3 bibliothèques utiles à la résolution des problèmes
-KM=pd.read_csv("EIVP_projet_1\EIVP_KMbis.csv") #il faut remplacer EIVP_projet_1\EIVP_KM.csv par ce qu'on a comme dossier
-print(KM.head(7880)) #pour s'assurer que le fichier est bien reconnu par le système
-print(KM) #pour s'assurer que le fichier est bien reconnu par le système et voir le nombre de colonne
+KM=pd.read_csv("EIVP_KMbis.csv",sep=';') #il faut remplacer EIVP_projet_1\EIVP_KM.csv par ce qu'on a comme dossier
+#print(KM.head(50)) #pour s'assurer que le fichier est bien reconnu par le système
+#print(KM['noise']) #pour s'assurer que le fichier est bien reconnu par le système et voir le nombre de colonne
+
+
+noise = []
+for element in KM['noise']:
+    noise.append(element)
+#print (noise)
+
+
+Temps = []
+k = 0
+for element in KM['sent_at']:
+    Temps.append(k)
+    k += 1
+#print (Temps)
+#trouver un meilleur moyen de faire les dates 
+
+plt.plot(Temps,noise)
+plt.show()
