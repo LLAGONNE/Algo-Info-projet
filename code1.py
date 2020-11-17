e7961d5a03a2457de9ebc5517fd7778200ce0d9f
jil3401 <jil3401@gmail.com>
2020-11-17 10:00:01 +0100

    mise à jour graphique du code

--- code1.py
+++ code1.py
@@ -3,22 +3,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from math import *
+sys.argv
 import sys #sys.argv pour permettre de pouvoir entrer les données sur powershell ou bien le terminal de windows
 KM = pd.read_csv ('EIVP_projet_1\EIVP_KMbis.csv' , sep=';') #il faut remplacer EIVP_projet_1\EIVP_KM.csv par ce qu'on a comme dossier
 #print(KMb.tail(60)['sent_at'])  #pour s'assurer que le fichier est bien reconnu par le système
-#print (KM['noise'][1])  #pour s'assurer que le fichier est bien reconnu par le système et voir le nombre de colonnes
+
+
 KMb=KM.sort_values(by = 'sent_at') #but travailler sur des valeurs deja triés(pas sur de pouvoir l'utiliser dans le doute):
-def point1 (colonne,start_at,end_at): 
+
+
+def point1 (colonne,start_at,end_at):
+     
     y = []
     Temps = []
     k=0
+    
     for i in range(len(KMb['sent_at'])): #idée de base créer une boucle pour representer le temps, ici idée est de pouvoir exprimer le départ et l'arrivée du temps
         if end_at==KMb['sent_at'][i][:10]:
             j=i
+            
         elif start_at==KMb['sent_at'][i][:10]:
             l=i
+            
     Temps=[k]
     y=[KMb[colonne][l]]
+    
     while KMb['sent_at'][l][:10]!=end_at:
         k+=1
         l+=1
@@ -125,4 +134,4 @@ def point2 (colonne): #les données du bruit sont fournies en dBA
         print ('La valeur médiane captée est de',med)
 
 #EXECUTION du programme:
-colonne=
+
